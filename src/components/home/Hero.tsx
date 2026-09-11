db0b99b866912c9656a7dca9da20f057043c3809
Favour Adejumo <favouradejumo223@gmail.com>
2026-09-11 08:19:21 +0100

    Change background image in Hero component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
   return (
     <section className="relative isolate min-h-[92vh] overflow-hidden">
       <img
-        src="/school-backdrop.png"
+        src="/students.png"
         alt="The Cornerstone Schools campus at sunset"
         fetchPriority="high"
         className="absolute inset-0 -z-10 size-full object-cover object-center"
